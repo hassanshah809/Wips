b2efbc1ac965ad32368ad9de88f0d8162683b036
kzhang098 <kz100@scarletmail.rutgers.edu>
2016-11-21 14:46:30 -0500

    Add files via upload

--- WIPS/src/model/parser/UserParser.java
+++ WIPS/src/model/parser/UserParser.java
@@ -21,87 +21,117 @@ public class UserParser extends Parser {
 	 * This list contains all usernames that have been found in the XML parser
 	 */
 		
-	ArrayList<String> userNames;
+	static ArrayList<String> userNames;
 	
 	/**
 	 * This hashmap contains a String regarding the "type of" error that was encountered while parsing.
 	 */
 	
-	HashMap<String,Boolean> keyMap; 
+	static HashMap<String,Boolean> keyMap; 
 	
-/**
- * This contains the intermediate object which contains the list of all
- * States and Entities associated with the "to-be-created" workflow.
- */
+	/**
+	 * This contains the intermediate object which contains the list of all
+	 * States and Entities associated with the "to-be-created" workflow.
+	 */
 
-WorkFlowInter<Entity,State> wfi;
+	static WorkFlowInter<Entity, State> wfi;
 
-/**
- * This contains the intermediate object which contains all user information
- * parsed from the user xml file.
- */
+	/**
+	 * This contains the intermediate object which contains all user information
+	 * parsed from the user xml file.
+	 */
 
-GenInter<User> usersInter = new GenInter<User>();
+	static GenInter<User> usersInter = new GenInter<User>();
 
-/**
- * it creates new user parser object
- */
-public UserParser(File userFile, WorkFlowInter wfi) {
-	super(userFile);
-	this.wfi = wfi;
-	keyMap = new HashMap<String,Boolean>();
-	keyMap.put("duplicateUserName", false);
-	keyMap.put("roleNotFound", false);
-	// TODO Auto-generated constructor stub
-}
-
-
-
-
-/**
- * This method is responsible for parsing the file containing all information regarding the user and stores the
- * parsed information in a GenInter<User> object to be used later.  
- * 
- */
-
-
-public void parse() {
-
-	try {
-
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		DocumentBuilder db = dbf.newDocumentBuilder();
-		Document doc = db.parse(fileName);
-		doc.getDocumentElement().normalize();
-
-		User user;
-
-		NodeList userList = doc.getElementsByTagName("user");
-
-for (int i = 0; i < userList.getLength(); i++) {
-
-	ArrayList<Entity> roleArrayList = new ArrayList<Entity>();
-	ArrayList<String> valuesArrayList = new ArrayList<String>();
-
-	Node userNode = userList.item(i);
-
-	String username = "";
-	String role = "";
-
-if (userNode.getNodeType() == Node.ELEMENT_NODE) {
-	Element userElement = (Element) userNode;
-	username = userElement.getAttribute("name");
-	
-	if(!userNames.contains(username)) {
-		userNames.add(username);
-	} else {
-		keyMap.put("duplicateUserName", true);
+	/**
+	 * it creates new user parser object
+	 */
+	public UserParser(File userFile, WorkFlowInter wfi) {
+		super(userFile);
+		this.wfi = wfi;
+		keyMap = new HashMap<String, Boolean>();
+		keyMap.put("duplicateUserName", false);
+		keyMap.put("roleNotFound", false);
+		// TODO Auto-generated constructor stub
 	}
 
-	Node node1 = userNode.getFirstChild();
-	Node node2 = node1.getNextSibling();
+	/**
+	 * This method is responsible for parsing the file containing all
+	 * information regarding the user and stores the parsed information in a
+	 * GenInter<User> object to be used later.
+	 * 
+	 */
 
-	if (node1.getNodeName().equals("roles")) {
+	public void parse() {
+
+		try {
+
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.parse(fileName);
+			doc.getDocumentElement().normalize();
+
+			User user;
+
+			NodeList userList = doc.getElementsByTagName("user");
+			
+			extractUsers(userList);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	private static void extractUsers(NodeList userList) {
+		
+		for (int i = 0; i < userList.getLength(); i++) {
+
+			ArrayList<Entity> roleArrayList = new ArrayList<Entity>();
+			ArrayList<String> valuesArrayList = new ArrayList<String>();
+
+			Node userNode = userList.item(i);
+
+			String username = "";
+			String role = "";
+
+			if (userNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element userElement = (Element) userNode;
+				username = userElement.getAttribute("name");
+
+				if (!userNames.contains(username)) {
+					userNames.add(username);
+				} else {
+					keyMap.put("duplicateUserName", true);
+				}
+
+				Node node1 = userNode.getFirstChild();
+				Node node2 = node1.getNextSibling();
+				
+			
+				//If the roles come first then I order node1 and then node2.
+				//Else then I would order node2 (Which should contain roles) and then node1 which should
+				//contain values. I'm doing this because the code handles roles first and then values. 
+			
+				if (node1.getNodeName().equals("roles")) {
+					handleChildren(node1,node2);
+				} else if(node1.getNodeName().equals("values")) {
+					handleChildren(node2,node1);
+				}	
+			}
+		}
+	}
+	
+	private static void handleChildren(Node node1, Node node2) {
+		
+		User user;
+		
+		ArrayList<Entity> roleArrayList = new ArrayList<Entity>();
+		ArrayList<String> valuesArrayList = new ArrayList<String>();
+		
+		String username = "";
+		String role = "";
+		
 		if (node1.getNodeType() == Node.ELEMENT_NODE) {
 			NodeList roleList = node1.getChildNodes();
 			Node roleNode;
@@ -115,24 +145,21 @@ if (userNode.getNodeType() == Node.ELEMENT_NODE) {
 					Entity entity;
 					ArrayList<Entity> entities = (ArrayList<Entity>) wfi.getTempAttr();
 					boolean roleFound = false;
-					
-					
+
 					for (int k = 0; k < entities.size(); k++) {
 						if (entities.get(k).getRole().equals(role)) {
 							roleArrayList.add(entities.get(k));
-							roleFound = true; 
-						} 
+							roleFound = true;
+						}
 					}
-					
-					if(!roleFound) {
-						
+
+					if (!roleFound) {
+
 					}
 				}
 			}
 		}
-
-		// NOW WE DO VALUES STUFF
-
+		
 		String value;
 
 		if (node2.getNodeType() == Node.ELEMENT_NODE) {
@@ -151,73 +178,21 @@ if (userNode.getNodeType() == Node.ELEMENT_NODE) {
 
 		user = new EndUser(username, roleArrayList, valuesArrayList);
 		usersInter.addAttr(user);
-	} else {
-
-		String value;
-
-		if (node1.getNodeType() == Node.ELEMENT_NODE) {
-			NodeList valuesList = node1.getChildNodes();
-			Node valueNode;
-
-			for (int j = 0; j < valuesList.getLength(); j++) {
-				valueNode = valuesList.item(j);
-				if (valueNode.getNodeType() == Node.ELEMENT_NODE) {
-					Element valueElement = (Element) valueNode;
-					value = valueElement.getAttribute("value");
-					valuesArrayList.add(value);
-				}
-			}
-		}
-
-		if (node2.getNodeType() == Node.ELEMENT_NODE) {
-			NodeList roleList = node2.getChildNodes();
-			Node roleNode;
-
-			for (int j = 0; j < roleList.getLength(); j++) {
-				roleNode = roleList.item(j);
-				if (roleNode.getNodeType() == Node.ELEMENT_NODE) {
-					Element roleElement = (Element) roleNode;
-					role = roleElement.getAttribute("value");
-
-					Entity entity;
-					ArrayList<Entity> entities = (ArrayList<Entity>) wfi.getTempAttr();
-
-					boolean roleFound = false;
-					
-					for (int k = 0; k < entities.size(); k++) {
-						if (entities.get(k).getRole().equals(role)) {
-							roleArrayList.add(entities.get(k));
-							roleFound = true; 
-						}
-					}
-					
-					if(!roleFound) {
-						keyMap.put("roleNotFound", true);
-					}
-				}
-			}
-						}
-					}
-
-					user = new EndUser(username, roleArrayList, valuesArrayList);
-					usersInter.addAttr(user);
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 	
+
 	/**
-	 * This returns the GenInter<User> object where the users parsed from the user file are stored. 
+	 * This returns the GenInter<User> object where the users parsed from the
+	 * user file are stored.
+	 * 
 	 * @return GenInter<User> usersInter.
 	 */
 
 	public GenInter<User> getUserInter() {
 		return this.usersInter;
-	
+
 	}
-	
+
 	@Override
 	public Object getInters() {
 		// TODO Auto-generated method stub
